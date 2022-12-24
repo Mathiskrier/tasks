e2faff568296cb4b0c7d6d357043b82a41a24d41
mathis Krier <mathis.krier@hotmail.fr>
2022-12-24 15:24:44 +0100

    checkbox for importance working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_080749) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_24_134515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_080749) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "importance"
+    t.boolean "importance", default: false
     t.boolean "completed", default: false
   end
+
 end
